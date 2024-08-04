b5a5788ef48304b02073dc8c6d37efebb52e49c4
Nikhil <nikhilraz05@gmail.com>
2024-08-04 23:47:50 +0530

    Initial commit

---
+++ src/componants/NewsItem.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+
+export class NewsItem extends Component {
+  render() {
+    let { title, description, imageUrl, newsUrl, author, date, source } =
+      this.props;
+    return (
+      <div className="my-3">
+        <div className="card">
+          <span
+            className="position-absolute top-0 translate-middle badge rounded-pill bg-danger"
+            style={{ zIndex: "1", left: "90%" }}
+          >
+            {source}
+          </span>
+          <img src={imageUrl} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <p className="card-text">
+              <small className="text-body-secondary">
+                By {!author ? "Unknown" : author} on{" "}
+                {new Date(date).toGMTString()}
+              </small>
+            </p>
+            <a
+              href={newsUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="btn sm btn-primary"
+            >
+              Read more...
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default NewsItem;
